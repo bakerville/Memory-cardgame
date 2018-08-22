08496e8d2890a6e88aef8d8ce655659e82a4be47
rlauderdale <rjlauderdale26@gmail.com>
2018-08-21 21:35:06 -0500

    refactored code
    refactored code. Still running into minor errors.
    
    --cards won't randomize
    --cards won't lock when matching pairs are found.

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
-function flipCard(){
-    this.classList.toggle('flip');
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-    if(!hasFlippedCard) {
-        //first click
-        hasFlippedCard = true;
-        firstCard= this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
+  this.classList.add('flip');
 
-        // do cards match?
-        if (firstCard.dataset.framework === secondCard.framework){
-            // it's a match!!
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            // not a match
-            setTimeout(() => {
-              firstCard.classList.remove('flip');
-              secondCard.classList.remove('flip'); 
-            }, 1500);
-            
-        }
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
 
+  secondCard = this;
+  lockBoard = true;
 
-    }
+  checkForMatch();
 }
 
-cards.forEach(card=> card.addEventListener('click', flipCard));
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let ramdomPos = Math.ceil(Math.random() * 12);
+    card.style.order = ramdomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
