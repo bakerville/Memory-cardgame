169165100703403abafe25de4f51af69b09bce83
rlauderdale <rjlauderdale26@gmail.com>
2018-08-21 20:35:59 -0500

    setting up a memory-cardgame
    html, css, and js files created.

---
+++ app.js
@@ -0,0 +1,35 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard(){
+    this.classList.toggle('flip');
+
+    if(!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard= this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // do cards match?
+        if (firstCard.dataset.framework === secondCard.framework){
+            // it's a match!!
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            // not a match
+            setTimeout(() => {
+              firstCard.classList.remove('flip');
+              secondCard.classList.remove('flip'); 
+            }, 1500);
+            
+        }
+
+
+    }
+}
+
+cards.forEach(card=> card.addEventListener('click', flipCard));
